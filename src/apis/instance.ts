7d178d9af5a4c69c781ac8532abd71d6d6c09e1c
wisdomin121@naver.com <wisdomin121@naver.com>
2023-09-10 21:12:53 +0900

    CHORE: json-server 배포 후, BASE_URL 변경

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8080/'
+const BASE_URL = 'https://wanted-pre-onboarding-frontend-4-json-server.vercel.app/'
 
 export const Instance = axios.create({ baseURL: BASE_URL })
