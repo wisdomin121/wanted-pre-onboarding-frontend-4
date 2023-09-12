cf4896430846ea64b8dd5fb4cf8da6dca3d8b4c2
wisdomin121@naver.com <wisdomin121@naver.com>
2023-09-12 15:13:02 +0900

    FEAT: 시계열 차트 만들기

--- src/components/TimeSeries.tsx
+++ src/components/TimeSeries.tsx
@@ -1,5 +1,81 @@
+import { useEffect, useState } from 'react'
+import {
+  Area,
+  Bar,
+  CartesianGrid,
+  ComposedChart,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+
+import { dataAPI } from 'apis/data'
+
+interface Data {
+  time: string
+  loc: string
+  valueArea: number
+  valueBar: number
+}
+
 function TimeSeries() {
-  return <></>
+  const [datas, setDatas] = useState<Data[]>([])
+
+  useEffect(() => {
+    dataAPI.get().then((res) => {
+      const unprocessedDatas = res.data.response
+      const processedDatas = []
+
+      for (const time in unprocessedDatas) {
+        const unprocessedData = unprocessedDatas[time]
+
+        processedDatas.push({
+          time: time.split(' ')[1],
+          loc: unprocessedData.id,
+          valueArea: unprocessedData.value_area,
+          valueBar: unprocessedData.value_bar,
+        })
+      }
+
+      setDatas(processedDatas)
+    })
+  }, [])
+
+  return (
+    <ResponsiveContainer width="100%" height={500}>
+      <ComposedChart
+        data={datas}
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20,
+        }}
+      >
+        <CartesianGrid stroke="#f5f5f5" />
+        <XAxis dataKey="time" scale="band" />
+        <YAxis
+          yAxisId="Area"
+          dataKey="valueArea"
+          orientation="left"
+          label={{ value: 'Area', angle: -90, position: 'insideLeft' }}
+          domain={[0, 200]}
+        />
+        <YAxis
+          yAxisId="Bar"
+          dataKey="valueBar"
+          orientation="right"
+          label={{ value: 'Bar', angle: -90, position: 'insideRight' }}
+        />
+        <Tooltip />
+        <Legend />
+        <Bar yAxisId="Bar" dataKey="valueBar" barSize={20} fill="#9EA1FE" />
+        <Area yAxisId="Area" type="monotone" dataKey="valueArea" fill="#EC8091" stroke="#EC8091" />
+      </ComposedChart>
+    </ResponsiveContainer>
+  )
 }
 
 export default TimeSeries
--- src/pages/MainPage.styled.ts
+++ src/pages/MainPage.styled.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components'
 
-export const MainStyled = styled.div``
+export const MainStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 97vh;
+`
